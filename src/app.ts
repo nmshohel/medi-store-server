6065fabbaf22e2c21a700f0ca6cf9da030809603
Nur Mohammad <nmshohel1992@gmail.com>
2026-01-27 00:14:44 +0600

    implementing better auth

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application } from "express";
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from 'cors';
 const app: Application = express();
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:3000", // client side url
+    credentials: true
+}))
 app.use(express.json());
-
-
+app.all('/api/auth/*splat', toNodeHandler(auth));
 app.get("/", (req, res) => {
-    res.send("Hello, MediStore! Server is running!");
+    res.send("Hello, World!");
 });
 
 export default app;
